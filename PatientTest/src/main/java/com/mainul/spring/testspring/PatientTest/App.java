5e2ec652e3c9d0f0641575759cb780d615d92dd7
maainul <mainul080@yahoo.com>
2020-02-11 17:25:53 +0600

    loading beans from the class path

--- PatientTest/src/main/java/com/mainul/spring/testspring/PatientTest/App.java
+++ PatientTest/src/main/java/com/mainul/spring/testspring/PatientTest/App.java
@@ -1,7 +1,8 @@
 package com.mainul.spring.testspring.PatientTest;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * Hello world!
@@ -11,10 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+    	ApplicationContext context = new ClassPathXmlApplicationContext("com/mainul/spring/testspring/PatientTest/beans/beans.xml");
     	Patient patient = (Patient)context.getBean("patient");
     	patient.speak();
         //System.out.println( "Hello World!" );
-    	((FileSystemXmlApplicationContext)context).close();
+    	((ClassPathXmlApplicationContext)context).close();
     }
 }
