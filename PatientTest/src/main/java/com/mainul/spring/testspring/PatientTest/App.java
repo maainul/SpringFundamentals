6c0c07eff2fd20f3027d483b3120d4fd0e47a7c1
maainul <mainul080@yahoo.com>
2020-02-11 17:39:18 +0600

    add value through constructor

--- PatientTest/src/main/java/com/mainul/spring/testspring/PatientTest/App.java
+++ PatientTest/src/main/java/com/mainul/spring/testspring/PatientTest/App.java
@@ -15,7 +15,10 @@ public class App
     	ApplicationContext context = new ClassPathXmlApplicationContext("com/mainul/spring/testspring/PatientTest/beans/beans.xml");
     	Patient patient = (Patient)context.getBean("patient");
     	patient.speak();
-        //System.out.println( "Hello World!" );
+        
+    	System.out.println(patient);
+    	
+    	
     	((ClassPathXmlApplicationContext)context).close();
     }
 }
--- PatientTest/src/main/java/com/mainul/spring/testspring/PatientTest/Patient.java
+++ PatientTest/src/main/java/com/mainul/spring/testspring/PatientTest/Patient.java
@@ -1,6 +1,61 @@
 package com.mainul.spring.testspring.PatientTest;
 
 public class Patient {
+	
+	private int id;
+	private String nameString;
+	
+	
+	public Patient() {
+		
+	}
+
+	
+	
+	public Patient(int id, String nameString) {
+		this.id = id;
+		this.nameString = nameString;
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getNameString() {
+		return nameString;
+	}
+
+
+
+
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", nameString=" + nameString + "]";
+	}
+
+
+
+
 	public void speak() {
 		System.out.println("Help me");
 	}
